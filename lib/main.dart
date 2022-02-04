2a24281e1c73f921d561d7f3d718ac784ad69a34
iori <iori@iori.dev>
2022-02-04 20:47:30 +0900

    fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
